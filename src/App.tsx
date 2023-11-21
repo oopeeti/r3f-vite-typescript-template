3655e5b5933e281e37d593d7c775674b8b31465d
Olli-Pekka Nikka <ollipekka.nikka@zoan.com>
2023-11-21 19:54:55 +0200

    project init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './index.css';
+
+function App() {
+  return (
+    <>
+      <div className="uppercase flex w-full h-full items-center justify-center">
+        Hello World
+      </div>
+    </>
+  );
+}
+
+export default App;
